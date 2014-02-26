897d4b4bcb79efd832f3c9bef8c2ede0502a8b5b
LOO2K <loo2km@gmail.com>
2014-02-26 11:23:52 +0800

    将二维码生成替换成本地生成 加快生成速度

--- GenerateQR.source/popup.js
+++ GenerateQR.source/popup.js
@@ -1,28 +1,33 @@
 
 function updateQR() {
-    chrome.tabs.getSelected(function(tab) {
-        console.log(tab);
+	chrome.tabs.getSelected(function(tab) {
+		console.log(tab);
 
-        // 检查是否存在二维码
-        var qrcode = document.getElementById('l2QRcode');
-        if( qrcode ) {
-            qrcode.parentNode.removeChild(qrcode);
-        }
+		// 检查是否存在二维码
+		var qrcode = document.getElementById('l2QRcode');
+		if( qrcode ) {
+			qrcode.parentNode.removeChild(qrcode);
+		}
 
-        // 声明 API
-        var api  = 'https://chart.googleapis.com/chart?cht=qr&chs=200x200&choe=UTF-8&chld=L|1&chl=';
-        var href = encodeURIComponent(tab.url);
-        var qr   = new Image();
-        qr.id = 'l2QRcode';
-        qr.src = api + href;
-        qr.height = '200';
-        qr.width = '200';
-        qr.setAttribute('title', tab.title);
-        document.body.appendChild(qr);
-        qr.onclick = function() {
-            window.close();
-        }
-    });
+		// 生成二维码
+		var dataURL = qr.toDataURL({
+			mime: 'image/jpeg',
+			value: tab.url,
+			size: 6
+		});
+
+		var qrimg = new Image();
+		qrimg.id = 'l2QRcode';
+		qrimg.src = dataURL;
+		// qrimg.height = '200';
+		// qrimg.width = '200';
+		qrimg.setAttribute('title', tab.title);
+		document.body.appendChild(qrimg);
+		qrimg.onclick = function() {
+			window.close();
+		};
+
+	});
 }
 
 chrome.browserAction.onClicked.addListener(updateQR);
